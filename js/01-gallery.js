b4e596516f4b2cdd29a46a52ccb72e3c35b927b3
Vitalii Chaban <vitaliichaban2022@gmail.com>
2023-01-09 04:25:34 -0500

    made 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+
+const containerGallery = document.querySelector('.gallery');
+const markup = imagesLists(galleryItems);
+
+containerGallery.insertAdjacentHTML("beforeend", markup);
+containerGallery.addEventListener('click', onContainerGallery); 
+
+    function imagesLists(galleryItems) {
+        return galleryItems.map(({preview, original, description})=>{
+            return ` 
+        <div class="gallery__item">
+        <a class="gallery__link" 
+        href="${original}">
+          <img class="gallery__image" 
+          src= "${preview}" 
+          data-source= "${original}" 
+          alt= "${description}" >
+        </a>
+        </div>
+        `;
+        })
+        .join('');
+    }
+    
+    function onContainerGallery(e){
+    e.preventDefault();
+
+    if(e.target.nodeName !== 'IMG'){
+        return
+    }
+    const instance = basicLightbox.create(`
+        <div class="modal">
+        <img src='${e.target.dataset.source}'>
+        </div>
+    `)
+    instance.show()
+    }  
